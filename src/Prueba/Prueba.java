7ac34e65cd4a6782d53d663d450a202fbe893082
carlitos <carlitosrechulon@gmail.com>
2022-11-29 18:25:32 +0100

    buenas tarde

--- src/Prueba/Prueba.java
+++ src/Prueba/Prueba.java
@@ -6,6 +6,6 @@ public static void main(String[] args) {
 	System.out.println("CARLOS");
 	String carlos;
 	System.out.println("hola");
-	String carlos;
+	System.out.println("hola buenas tardes");
 }
 }
